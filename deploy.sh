2bc4b81fedb6907dbe84deec83d5b376bb0619ca
ec2-user <nvaisreal@yahoo.com>
2022-05-23 12:18:09 +0000

    Tested and approved by the client QA Team
    file deploy.sh was modified to include developes file
    files .gitignore and git,ignore were also both modified

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 deploying java application
+deploying the application file
+deployment 4
+deploy to staging enviroment
+echo good job
+tested and accepted 
+(Tested and approved by the client QA Team)
